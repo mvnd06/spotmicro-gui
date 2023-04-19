3f9fc3c244c0c6f3eb1725ce92efa3708581f8cc
Armand Raynor <13355630+mvnd06@users.noreply.github.com>
2023-04-19 11:00:37 -0700

    Add Status Bar

--- index.js
+++ index.js
@@ -3,6 +3,39 @@ const socket = new WebSocket('ws://localhost:8080');
 
 // When a message is received on the WebSocket connection, update the visualization
 socket.addEventListener('message', (event) => {
-  const data = JSON.parse(event.data);
-  console.log(data);
+  const message = JSON.parse(event.data);
+  // console.log(data);
+  if (message.topic == 'status') {
+    processStatusMessage(message.data)
+  }
 });
+
+function processStatusMessage(statusData) {
+  console.log('status received')
+  console.log(statusData)
+  const motionStatus = statusData.motion ? 'running' : 'stopped';
+  const displayStatus = statusData.display ? 'running' : 'stopped';
+  const guiStatus = statusData.gui ? 'running' : 'stopped';
+
+  const motionCircle = document.getElementById('motion-status-label');
+  const displayCircle = document.getElementById('display-status-label');
+  const guiCircle = document.getElementById('gui-status-label');
+
+  if (motionStatus === 'running') {
+    motionCircle.style.backgroundColor = 'green';
+  } else {
+    motionCircle.style.backgroundColor = 'red';
+  }
+
+  if (displayStatus === 'running') {
+    displayCircle.style.backgroundColor = 'green';
+  } else {
+    displayCircle.style.backgroundColor = 'red';
+  }
+
+  if (guiStatus === 'running') {
+    guiCircle.style.backgroundColor = 'green';
+  } else {
+    guiCircle.style.backgroundColor = 'red';
+  }
+}
--- server.js
+++ server.js
@@ -25,6 +25,21 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+// Initialize status variables
+const status = {
+  motion: true,
+  display: true,
+  gui: true
+};
+
+// Load resources
+app.use(express.static('public'));
+
+// Serve the status-bar.js file to the client
+app.get('/status-bar.js', (req, res) => {
+  res.sendFile(path.join(__dirname, 'status-bar.js'));
+});
+
 wss.on('connection', (socket) => {
     console.log('WebSocket connected');
 
@@ -37,15 +52,58 @@ wss.on('connection', (socket) => {
 
     // When a message is received on the "ultrasonic_data" topic, send the data to the front-end
     ultrasonicDataListener.subscribe((message) => {
-        const data = {
+        const ultrasonicData = {
             topic: ultrasonicDataListener.name,
-            values: message.data
+            data: message.data
           };          
-        socket.send(JSON.stringify(data));
+        socket.send(JSON.stringify(ultrasonicData));
     });
+
+    // Send initial status on connection
+    const statusData = {
+      topic: 'status',
+      data: status
+    }
+    socket.send(JSON.stringify(statusData));
 });
 
 // Start server
 server.listen(8080, 'localhost', () => {
   console.log('Server started on localhost:8080');
 });
+
+// Get the status of each service
+function getStatus() {
+  // const motion = spawn('systemctl', ['is-active', 'motion']);
+  // motion.stdout.on('data', (data) => {
+  //   motionStatus = data.toString().trim() === 'active';
+  // });
+
+  // const display = spawn('systemctl', ['is-active', 'display']);
+  // display.stdout.on('data', (data) => {
+  //   displayStatus = data.toString().trim() === 'active';
+  // });
+
+  // const gui = spawn('systemctl', ['is-active', 'gui']);
+  // gui.stdout.on('data', (data) => {
+  //   guiStatus = data.toString().trim() === 'active';
+
+  //   // Emit status update over WebSocket connection
+  //   wss.clients.forEach((client) => {
+  //     client.send(JSON.stringify({
+  //       type: 'status',
+  //       motion: motionStatus,
+  //       display: displayStatus,
+  //       gui: guiStatus
+  //     }));
+  //   });
+  // });
+}
+
+// Get status on startup
+getStatus();
+
+// Poll for status updates every 5 seconds
+setInterval(() => {
+  getStatus();
+}, 5000);
