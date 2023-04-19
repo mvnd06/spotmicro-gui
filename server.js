ef1951c084394469b0574f7f473e587560a9f20f
mvnd06 <armand_r@icloud.com>
2023-04-19 09:23:45 -0700

    Implement ultrasonic topic

--- server.js
+++ server.js
@@ -25,66 +25,27 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-wss.on('connection', (ws) => {
-  console.log('WebSocket connected');
+wss.on('connection', (socket) => {
+    console.log('WebSocket connected');
 
-  // Subscribe to ROS topics
-  const statusTopic = new ROSLIB.Topic({
-    ros: ros,
-    name: '/robot/status',
-    messageType: 'std_msgs/String'
-  });
-
-  statusTopic.subscribe((message) => {
-    ws.send(JSON.stringify({
-      type: 'status',
-      data: message.data
-    }));
-  });
-
-  const sensorsTopic = new ROSLIB.Topic({
-    ros: ros,
-    name: '/robot/sensors',
-    messageType: 'std_msgs/String'
-  });
-
-  sensorsTopic.subscribe((message) => {
-    ws.send(JSON.stringify({
-      type: 'sensors',
-      data: message.data
-    }));
-  });
-
-  // Listen for commands from front-end
-  ws.on('message', (message) => {
-    const command = JSON.parse(message);
-
-    if (command.type === 'cmd_vel') {
-      const cmdVelTopic = new ROSLIB.Topic({
+    // Subscribe to ROS topics
+    const ultrasonicDataListener = new ROSLIB.Topic({
         ros: ros,
-        name: '/robot/cmd_vel',
-        messageType: 'geometry_msgs/Twist'
-      });
+        name: '/ultrasonic_data',
+        messageType: 'std_msgs/Int32MultiArray'
+    });
 
-      const twist = new ROSLIB.Message({
-        linear: {
-          x: command.data.linear.x,
-          y: command.data.linear.y,
-          z: command.data.linear.z
-        },
-        angular: {
-          x: command.data.angular.x,
-          y: command.data.angular.y,
-          z: command.data.angular.z
-        }
-      });
-
-      cmdVelTopic.publish(twist);
-    }
-  });
+    // When a message is received on the "ultrasonic_data" topic, send the data to the front-end
+    ultrasonicDataListener.subscribe((message) => {
+        const data = {
+            topic: ultrasonicDataListener.name,
+            values: message.data
+          };          
+        socket.send(JSON.stringify(data));
+    });
 });
 
 // Start server
-server.listen(8080, () => {
-  console.log('Server started on port 8080');
+server.listen(8080, 'localhost', () => {
+  console.log('Server started on localhost:8080');
 });
